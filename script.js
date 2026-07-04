0594c09ae3cb9322a509b56b34ead169e5f171b7
Princess Rakhunwana <priethee3xplora@gmail.com>
2026-07-04 02:20:20 +0200

    done - p.s sort out music issue if nesesry

--- script.js
+++ script.js
@@ -30,3 +30,7 @@ window.addEventListener('load', () => {
         music.play();
     }
 });Interval(createFlower, 300);
+
+localStorage.setItem("musicTime", audio.currentTime);
+audio.currentTime=localStorage.getItem("musicTime") || 0;
+audio.play();
